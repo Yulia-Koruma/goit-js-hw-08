eaf6018c12ca6b04258e3b59a803ff15bb75193b
Yulia Koruma <90381665+Yulia-Koruma@users.noreply.github.com>
2022-01-29 00:09:35 +0200

    task2-done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,13 +2,14 @@ import throttle from "lodash.throttle";
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
-    player.on('play', function() {
-        console.log('played the video!');
-    });
-
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
-
+const STORAGE_KEY = 'videoplayer-current-time';
     
+const onPlay = function (data) {
+    localStorage.setItem(STORAGE_KEY, data.seconds)
+};
+
+const startPlay = localStorage.getItem(STORAGE_KEY);
+const setTime = startPlay ? startPlay: 0;
+player.setCurrentTime(setTime);
+
+player.on('timesaved', throttle(onPlay, 1000));
